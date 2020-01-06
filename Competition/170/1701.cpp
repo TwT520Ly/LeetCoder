dbf45b1d9b8ddee777073956731b1887bdf714e1
谭文韬 <tan.wt@aiiage.com>
2020-01-06 17:48:12 +0800

    周赛170第一道题

--- Competition/170/1701.cpp
+++ Competition/170/1701.cpp
@@ -9,10 +9,7 @@ int main() {
         string temp;
         for(int i = 0; i < s.length(); i ++) {
             if((i+2) < s.length() && s[i+2] == '#') {
-                string t = "";
-                t += s[i];
-                t += s[i+1];
-                temp += (96 + atoi(t.c_str()));
+                temp += (96 + atoi(s.substr(i, 2).c_str()));
                 i += 2;
             } else
                 temp += (96 + s[i] - 48);
